fab5b3ceb75aebdb7ec75b7b7f72beb017278628
Nicole <yh397@cornell.edu>
2025-06-12 02:16:34 -0400

    first commit, main.py to initialize environment variables and set recursion limit

---
+++ main.py
@@ -0,0 +1,9 @@
+from colorama import Fore, Style
+from src.graph import Workflow
+from dotenv import load_dotenv
+
+# Load all env variables
+load_dotenv()
+
+# config 
+config = {'recursion_limit': 100}
